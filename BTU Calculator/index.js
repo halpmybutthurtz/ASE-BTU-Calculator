3875ec71fb7fde9ae4178e868ee8540ea0806226
halpmybutthurtz <henrysproperemail@gmail.com>
2024-05-20 01:17:59 -0400

    Add files via upload
    Added devices per circuit table
    Added stylization

--- BTU Calculator/index.js
+++ BTU Calculator/index.js
@@ -7,13 +7,93 @@ let totalBtuBox = document.getElementById("total-btu25")
 let totalBtuMaxBox = document.getElementById("total-btu")
 let totalRmsIdleBox = document.getElementById("total-rms-idle")
 let totalRmsLongTermBox = document.getElementById("total-rms-lt")
+let maxPerCircuit = document.getElementById("max-values")
+
+//Device Objects
+let csDevices = [
+    {
+        name: cs119,
+        current: [2.15, 2.25, 2.38, 4.12, 4.3, 4.5, 4.95],
+        idleCurrent: [0.45, 0.47, 0.5, 0.86, 0.9, 0.96, 1.04],
+        maxQuantity: [7, 6, 6, 3, 3, 3, 3],
+        quantity: document.getElementById("cs119"),
+        btuBox: document.getElementById("cs119-btu25"),
+        btuMax: document.getElementById("cs119-btu"),
+        rmsIdleBox: document.getElementById("cs119-rms-idle"),
+        rmsLongTermBox: document.getElementById("cs119-rms-lt")
+    },
+    
+    {
+        name: cs118,
+        current: [1.25, 1.31, 1.38, 2.4, 2.5, 2.62, 2.88],
+        idleCurrent: [0.45, 0.47, 0.5, 0.86, 0.9, 0.96, 1.04],
+        maxQuantity: [11, 11, 10, 6, 6, 5, 5],
+        quantity: document.getElementById("cs118"),
+        btuBox: document.getElementById("cs118-btu25"),
+        btuMax: document.getElementById("cs118-btu"),
+        rmsIdleBox: document.getElementById("cs118-rms-idle"),
+        rmsLongTermBox: document.getElementById("cs118-rms-lt")
+    },
+    
+    {
+        name: cs7,
+        current: [1.6, 1.68, 1.78, 3.1, 3.22, 3.36, 3.7],
+        idleCurrent: [0.45, 0.47, 0.5, 0.86, 0.9, 0.96, 1.04],
+        maxQuantity: [9, 8, 8, 4, 4, 4, 4],
+        quantity: document.getElementById("cs7"),
+        btuBox: document.getElementById("cs7-btu25"),
+        btuMax: document.getElementById("cs7-btu"),
+        rmsIdleBox: document.getElementById("cs7-rms-idle"),
+        rmsLongTermBox: document.getElementById("cs7-rms-lt")
+    },
+    
+    {
+        name: cs10,
+        current: [1.6, 1.68, 1.78, 3.1, 3.22, 3.36, 3.7],
+        idleCurrent: [0.45, 0.47, 0.5, 0.86, 0.9, 0.96, 1.04],
+        maxQuantity: [9, 8, 8, 4, 4, 4, 4],
+        quantity: document.getElementById("cs10"),
+        rmsIdleBox: document.getElementById("cs10-rms-idle"),
+        btuBox: document.getElementById("cs10-btu25"),
+        btuMax: document.getElementById("cs10-btu"),
+        rmsIdleBox: document.getElementById("cs10-rms-idle"),
+        rmsLongTermBox: document.getElementById("cs10-rms-lt")
+    },
+    
+    {
+        name: cs7p,
+        current: [1.6, 1.68, 1.78, 3.1, 3.22, 3.36, 3.7],
+        idleCurrent: [0.45, 0.47, 0.5, 0.86, 0.9, 0.96, 1.04],
+        maxQuantity: [9, 8, 8, 4, 4, 4, 4],
+        quantity: document.getElementById("cs7p"),
+        btuBox: document.getElementById("cs7p-btu25"),
+        btuMax: document.getElementById("cs7p-btu"),
+        rmsIdleBox: document.getElementById("cs7p-rms-idle"),
+        rmsLongTermBox: document.getElementById("cs7p-rms-lt")
+    },
+    
+    {
+        name: cs10p,
+        current: [1.6, 1.68, 1.78, 3.1, 3.22, 3.36, 3.7],
+        idleCurrent: [0.45, 0.47, 0.5, 0.86, 0.9, 0.96, 1.04],
+        maxQuantity: [9, 8, 8, 4, 4, 4, 4],
+        quantity: document.getElementById("cs10p"),
+        btuBox: document.getElementById("cs10p-btu25"),
+        btuMax: document.getElementById("cs10p-btu"),
+        rmsIdleBox: document.getElementById("cs10p-rms-idle"),
+        rmsLongTermBox: document.getElementById("cs10p-rms-lt")
+    }]
 
 //Voltage and Current Selector
 function setVoltage(v) {
     voltageIndex = lineVoltage[v]
     arraySelector = v
     console.log(voltageIndex, arraySelector)
+    altBtuH()
+    getMax()
 }
+
+setVoltage(0)
 
 //Clear Button
 function clearFields() {
@@ -21,6 +101,18 @@ function clearFields() {
         csDevices[q].quantity.value = 0
     }
     altBtuH()
+}
+
+//Max Per Circuit
+function getMax() {
+    maxPerCircuit.innerHTML = `
+        <td>${csDevices[0].maxQuantity[arraySelector]}</td>
+        <td>${csDevices[1].maxQuantity[arraySelector]}</td>
+        <td>${csDevices[2].maxQuantity[arraySelector]}</td>
+        <td>${csDevices[3].maxQuantity[arraySelector]}</td>
+        <td>${csDevices[4].maxQuantity[arraySelector]}</td>
+        <td>${csDevices[5].maxQuantity[arraySelector]}</td>
+        `
 }
 
 //Calculator
@@ -47,28 +139,6 @@ function altBtuH() {
     }
 }
 
-//Old Method
-/*function btuH(device) {
-    let deviceCurrent = csDevices[device].current[arraySelector]
-    let deviceQuantity = csDevices[device].quantity.value
-    let deviceIdle = csDevices[device].idleCurrent[arraySelector]
-    let idleBox = csDevices[device].rmsIdleBox
-    let longTermBox = csDevices[device].rmsLongTermBox
-    let btuBox = csDevices[device].btuBox
-    let btuMaxBox = csDevices[device].btuMax
-
-    let watts = deviceCurrent * voltageIndex
-    let energy = watts * time
-    let btupH = energy / 1055
-    let max = btupH * 0.25
-    let btu = deviceQuantity * max
-    btuBox.value = btu.toFixed(2)
-    idleBox.value = (deviceIdle * deviceQuantity).toFixed(2)
-    longTermBox.value = (deviceCurrent * deviceQuantity).toFixed(2)
-    btuMaxBox.value = (btu * 4).toFixed(2)
-    sum()
-}*/
-
 //Totals Calculations
 function sum() {
     let totalBtu = 0
@@ -86,78 +156,3 @@ function sum() {
     totalRmsIdleBox.value = totalRmsIdle.toFixed(2)
     totalRmsLongTermBox.value = totalRmsLongTerm.toFixed(2)
 }
-
-//Device Objects
-let csDevices = [
-{
-    name: cs119,
-    current: [2.15, 2.25, 2.38, 4.12, 4.3, 4.5, 4.95],
-    idleCurrent: [0.45, 0.47, 0.5, 0.86, 0.9, 0.96, 1.04],
-    maxQuantity: [7, 6, 6, 3, 3, 3, 3],
-    quantity: document.getElementById("cs119"),
-    btuBox: document.getElementById("cs119-btu25"),
-    btuMax: document.getElementById("cs119-btu"),
-    rmsIdleBox: document.getElementById("cs119-rms-idle"),
-    rmsLongTermBox: document.getElementById("cs119-rms-lt")
-},
-
-{
-    name: cs118,
-    current: [1.25, 1.31, 1.38, 2.4, 2.5, 2.62, 2.88],
-    idleCurrent: [0.45, 0.47, 0.5, 0.86, 0.9, 0.96, 1.04],
-    maxQuantity: [11, 11, 10, 6, 6, 5, 5],
-    quantity: document.getElementById("cs118"),
-    btuBox: document.getElementById("cs118-btu25"),
-    btuMax: document.getElementById("cs118-btu"),
-    rmsIdleBox: document.getElementById("cs118-rms-idle"),
-    rmsLongTermBox: document.getElementById("cs118-rms-lt")
-},
-
-{
-    name: cs7,
-    current: [1.6, 1.68, 1.78, 3.1, 3.22, 3.36, 3.7],
-    idleCurrent: [0.45, 0.47, 0.5, 0.86, 0.9, 0.96, 1.04],
-    maxQuantity: [9, 8, 8, 4, 4, 4, 4],
-    quantity: document.getElementById("cs7"),
-    btuBox: document.getElementById("cs7-btu25"),
-    btuMax: document.getElementById("cs7-btu"),
-    rmsIdleBox: document.getElementById("cs7-rms-idle"),
-    rmsLongTermBox: document.getElementById("cs7-rms-lt")
-},
-
-{
-    name: cs10,
-    current: [1.6, 1.68, 1.78, 3.1, 3.22, 3.36, 3.7],
-    idleCurrent: [0.45, 0.47, 0.5, 0.86, 0.9, 0.96, 1.04],
-    maxQuantity: [9, 8, 8, 4, 4, 4, 4],
-    quantity: document.getElementById("cs10"),
-    rmsIdleBox: document.getElementById("cs10-rms-idle"),
-    btuBox: document.getElementById("cs10-btu25"),
-    btuMax: document.getElementById("cs10-btu"),
-    rmsIdleBox: document.getElementById("cs10-rms-idle"),
-    rmsLongTermBox: document.getElementById("cs10-rms-lt")
-},
-
-{
-    name: cs7p,
-    current: [1.6, 1.68, 1.78, 3.1, 3.22, 3.36, 3.7],
-    idleCurrent: [0.45, 0.47, 0.5, 0.86, 0.9, 0.96, 1.04],
-    maxQuantity: [9, 8, 8, 4, 4, 4, 4],
-    quantity: document.getElementById("cs7p"),
-    btuBox: document.getElementById("cs7p-btu25"),
-    btuMax: document.getElementById("cs7p-btu"),
-    rmsIdleBox: document.getElementById("cs7p-rms-idle"),
-    rmsLongTermBox: document.getElementById("cs7p-rms-lt")
-},
-
-{
-    name: cs10p,
-    current: [1.6, 1.68, 1.78, 3.1, 3.22, 3.36, 3.7],
-    idleCurrent: [0.45, 0.47, 0.5, 0.86, 0.9, 0.96, 1.04],
-    maxQuantity: [9, 8, 8, 4, 4, 4, 4],
-    quantity: document.getElementById("cs10p"),
-    btuBox: document.getElementById("cs10p-btu25"),
-    btuMax: document.getElementById("cs10p-btu"),
-    rmsIdleBox: document.getElementById("cs10p-rms-idle"),
-    rmsLongTermBox: document.getElementById("cs10p-rms-lt")
-}]
